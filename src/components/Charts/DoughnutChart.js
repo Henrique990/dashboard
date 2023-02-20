bc44d3a6e6689a427ed1822d93f6a35b3089df0c
Henrique <henriqueteixeira26@gmail.com>
2023-02-20 13:43:10 -0300

    grafigo Pie Completo

--- src/components/Charts/DoughnutChart.js
+++ src/components/Charts/DoughnutChart.js
@@ -1,10 +1,5 @@
-import React, { useRef} from "react";
-import { Chart as 
-  ChartJS, 
-  ArcElement, 
-  Tooltip, 
-  Legend 
-} from "chart.js";
+import React, { useRef, useState } from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
 import { Pie, getElementsAtEvent } from "react-chartjs-2";
 import custoJson from "../../custoJson.json";
@@ -55,19 +50,13 @@ const config2 = {
   },
 };
 
-const chartRef = useRef
-const onClick = (event) => {
-  console.log(getElementsAtEvent(chartRef.current, event));
-}
-
-
 // const labels = ['Fazenda Alvorada', 'Fazenda da Conquista']
 
 export const data = {
   labels: ["Fazenda Alvorada", "Fazenda da Conquista"],
   datasets: [
     {
-      label: 'Fazendas',
+      label: "Fazendas",
       data: [totalCusto34, totalCusto35],
       backgroundColor: ["blue", "green"],
       borderColor: ["black"],
@@ -77,19 +66,50 @@ export const data = {
   ],
 };
 const servicos = custoJson.map((data) => data.servicos);
+const servicos34 = fazenda34.map((data) => data.servicos);
+const servicos35 = fazenda35.map((data) => data.servicos);
+console.log(servicos34);
 
 const tipoInsumos = servicos.map((data) =>
   data.filter((data) => data.tipo === "insumos")
 );
+const tipoInsumos34 = servicos34.map((data) =>
+  data.filter((data) => data.tipo === "insumos")
+);
+const tipoInsumos35 = servicos35.map((data) =>
+  data.filter((data) => data.tipo === "insumos")
+);
+
 const tipoMaoObra = servicos.map((data) =>
   data.filter((data) => data.tipo === "maoObra")
 );
+const tipoMaoObra34 = servicos34.map((data) =>
+  data.filter((data) => data.tipo === "maoObra")
+);
+const tipoMaoObra35 = servicos35.map((data) =>
+  data.filter((data) => data.tipo === "maoObra")
+);
+
 const tipoFrota = servicos.map((data) =>
   data.filter((data) => data.tipo === "frota")
 );
+const tipoFrota34 = servicos34.map((data) =>
+  data.filter((data) => data.tipo === "frota")
+);
+const tipoFrota35 = servicos35.map((data) =>
+  data.filter((data) => data.tipo === "frota")
+);
+
 const tipoRateio = servicos.map((data) =>
   data.filter((data) => data.tipo === "rateio")
 );
+const tipoRateio34 = servicos34.map((data) =>
+  data.filter((data) => data.tipo === "rateio")
+);
+const tipoRateio35 = servicos35.map((data) =>
+  data.filter((data) => data.tipo === "rateio")
+);
+
 // console.log(tipoInsumos)
 // console.log(tipoMaoObra)
 // console.log(tipoFrota)
@@ -97,15 +117,43 @@ const tipoRateio = servicos.map((data) =>
 const valorInsumos = tipoInsumos.map((data) =>
   data.reduce((acc, current) => acc + current.valor, 0)
 );
+const valorInsumos34 = tipoInsumos34.map((data) =>
+  data.reduce((acc, current) => acc + current.valor, 0)
+);
+const valorInsumos35 = tipoInsumos35.map((data) =>
+  data.reduce((acc, current) => acc + current.valor, 0)
+);
+//----------------------------------------------------------------
 const valorMaoObra = tipoMaoObra.map((data) =>
   data.reduce((acc, current) => acc + current.valor, 0)
 );
+const valorMaoObra34 = tipoMaoObra34.map((data) =>
+  data.reduce((acc, current) => acc + current.valor, 0)
+);
+const valorMaoObra35 = tipoMaoObra35.map((data) =>
+  data.reduce((acc, current) => acc + current.valor, 0)
+);
+//-----------------------------------------------------------------
 const valorFrota = tipoFrota.map((data) =>
   data.reduce((acc, current) => acc + current.valor, 0)
 );
+const valorFrota34 = tipoFrota34.map((data) =>
+  data.reduce((acc, current) => acc + current.valor, 0)
+);
+const valorFrota35 = tipoFrota35.map((data) =>
+  data.reduce((acc, current) => acc + current.valor, 0)
+);
+//----------------------------------------------------------------
 const valorRateio = tipoRateio.map((data) =>
   data.reduce((acc, current) => acc + current.valor, 0)
 );
+const valorRateio34 = tipoRateio34.map((data) =>
+  data.reduce((acc, current) => acc + current.valor, 0)
+);
+const valorRateio35 = tipoRateio35.map((data) =>
+  data.reduce((acc, current) => acc + current.valor, 0)
+);
+//----------------------------------------------------------------
 // console.log(valorInsumos)
 // console.log(valorMaoObra)
 // console.log(valorFrota)
@@ -114,12 +162,48 @@ const totalInsumos = valorInsumos.reduce(
   (sum, valorInsumos) => sum + valorInsumos,
   0
 );
+const totalInsumos34 = valorInsumos34.reduce(
+  (sum, valorInsumos) => sum + valorInsumos,
+  0
+);
+const totalInsumos35 = valorInsumos35.reduce(
+  (sum, valorInsumos) => sum + valorInsumos,
+  0
+);
+//----------------------------------------------------------------
 const totalMaoObra = valorMaoObra.reduce((sum, valorMao) => sum + valorMao, 0);
+const totalMaoObra34 = valorMaoObra34.reduce(
+  (sum, valorMao) => sum + valorMao,
+  0
+);
+const totalMaoObra35 = valorMaoObra35.reduce(
+  (sum, valorMao) => sum + valorMao,
+  0
+);
+//----------------------------------------------------------------
 const totalFrota = valorFrota.reduce((sum, valorFrota) => sum + valorFrota, 0);
+const totalFrota34 = valorFrota34.reduce(
+  (sum, valorFrota) => sum + valorFrota,
+  0
+);
+const totalFrota35 = valorFrota35.reduce(
+  (sum, valorFrota) => sum + valorFrota,
+  0
+);
+//----------------------------------------------------------------
 const totalRateio = valorRateio.reduce(
   (sum, valorRateio) => sum + valorRateio,
   0
 );
+const totalRateio34 = valorRateio34.reduce(
+  (sum, valorRateio) => sum + valorRateio,
+  0
+);
+const totalRateio35 = valorRateio35.reduce(
+  (sum, valorRateio) => sum + valorRateio,
+  0
+);
+
 // console.log(totalInsumos)
 // console.log(totalMaoObra)
 // console.log(totalFrota)
@@ -138,13 +222,60 @@ export const data2 = {
 };
 
 export const DoughnutChart = () => {
+  let [dataValueForChart2, setDataValueForChart2] = useState(data2);
+
+  const chartRef = useRef();
+  const onClick = (event) => {
+    if (getElementsAtEvent(chartRef.current, event).length > 0) {
+      console.log(getElementsAtEvent(chartRef.current, event));
+      const datasetIndexNum = getElementsAtEvent(chartRef.current, event)[0]
+        .datasetIndex;
+      const dataPoint = getElementsAtEvent(chartRef.current, event)[0].index;
+
+      console.log(`Dataset: ${datasetIndexNum} and Data: ${dataPoint}`);
+      const selectedLabel = data.labels[dataPoint];
+      console.log(selectedLabel);
+
+      let newData2;
+      if (selectedLabel === "Fazenda Alvorada") {
+        newData2 = {
+          labels: ["Mão de Obra", "Frota", "Insumos", "Rateio"],
+          datasets: [
+            {
+              data: [totalMaoObra35, totalFrota35, totalInsumos35, totalRateio35],
+              backgroundColor: ["#FF6347", "#4682B4", "#fc5c9c", "#FFA500"],
+              borderColor: ["black"],
+              borderWidth: 1,
+              hoverOffset: 16,
+            },
+          ],
+        };
+      } else if(selectedLabel === 'Fazenda da Conquista') {
+        newData2 = {
+          labels: ["Mão de Obra", "Frota", "Insumos", "Rateio"],
+          datasets: [
+            {
+              data: [totalMaoObra34, totalFrota34, totalInsumos34, totalRateio34],
+              backgroundColor: ["#FF6347", "#4682B4", "#fc5c9c", "#FFA500"],
+              borderColor: ["black"],
+              borderWidth: 1,
+              hoverOffset: 16,
+            },
+          ],
+        };
+      }
+      setDataValueForChart2(newData2);
+      console.log(setDataValueForChart2)
+    }
+  };
+
   return (
     <CardContent sx={{ display: "flex", justifyContent: "space-around" }}>
       <Box sx={{ maxHeight: "20rem" }}>
         <Pie options={config} data={data} onClick={onClick} ref={chartRef} />
       </Box>
       <Box sx={{ maxHeight: "20rem" }}>
-        <Pie options={config2} data={data2} />
+        <Pie options={config2} data={dataValueForChart2} />
       </Box>
     </CardContent>
   );
